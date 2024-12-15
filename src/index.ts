e13154f3a65b6b7db804f5761fb5808c00368973
Christian Nunciato <chris@nunciato.org>
2024-12-15 08:30:25 -0800

    Generate types from schema

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import Environment from "./environment";
 import StepBuilder from "./stepBuilder";
 
-export * as types from "./types";
-
 export { Environment, StepBuilder };
--- test/hello.test.ts
+++ test/hello.test.ts
@@ -1,3 +1,17 @@
-test("hello", () => {
-    expect(true).toBe(true);
+import * as buildkite from "../src";
+
+test("pipeline", () => {
+    const pipeline = new buildkite.StepBuilder();
+
+    pipeline.addStep({
+        type: "command",
+        command: "echo 'Hello, world!'",
+    });
+
+    pipeline.addStep({
+        type: "block",
+        prompt: "Continue?",
+    });
+
+    expect(pipeline.toJSON()).toBeTruthy;
 });
