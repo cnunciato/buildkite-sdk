bf6b62f7ceab7068a2822aa9087e5ca46a7bf1b4
Christian Nunciato <chris@nunciato.org>
2024-12-27 10:08:04 -0800

    All passing but Py

--- libs/sdk/typescript/src/sdk.spec.ts
+++ libs/sdk/typescript/src/sdk.spec.ts
@@ -2,6 +2,8 @@ import * as buildkite from "./sdk";
 
 describe("toJSON()", () => {
     it("should work", () => {
-        expect(new buildkite.Pipeline().toJSON()).toBe({ steps: [] });
+        expect(new buildkite.Pipeline().toJSON()).toBe(
+            JSON.stringify({ steps: [] }, null, 4)
+        );
     });
 });
