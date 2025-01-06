21476fc4540f00238971a2dfde399e80865a3a8f
Christian Nunciato <chris@nunciato.org>
2025-01-05 20:31:25 -0800

    Echo the vals

--- .buildkite/pipeline.rb
+++ .buildkite/pipeline.rb
@@ -2,13 +2,16 @@ require_relative("../sdk/ruby/lib/buildkite.rb")
 require_relative("../sdk/ruby/lib/environment.rb")
 
 pipeline = Buildkite::Pipeline.new
+tag = ENV[Environment::BUILDKITE_TAG]
 
 commands = [
   "npm test",
-  "npm run build"
+  "npm run build",
+  "echo '#{Environment::BUILDKITE_TAG}'",
+  "echo '#{tag}'"
 ]
 
-if not ENV[Environment::BUILDKITE_TAG].nil?
+if not tag.nil? and not tag == ""
   commands.push("npm run publish")
 end
 
