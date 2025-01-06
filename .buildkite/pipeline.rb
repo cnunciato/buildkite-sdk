d3b3cac5ff48e51b631484731f249c225387ea89
Christian Nunciato <chris@nunciato.org>
2025-01-05 20:01:28 -0800

    Try identifying tags

--- .buildkite/pipeline.rb
+++ .buildkite/pipeline.rb
@@ -2,16 +2,19 @@ require_relative("../sdk/ruby/lib/buildkite.rb")
 require_relative("../sdk/ruby/lib/environment.rb")
 
 pipeline = Buildkite::Pipeline.new
+
+commands = [
+  "npm test",
+  "npm run build"
+]
+
+if ENV[Environment::BUILDKITE_TAG]
+  commands.push("npm run publish")
+end
+
 pipeline.add_command_step(
   label: ":hammer_and_wrench: Install, test, and build",
-  commands: [
-    "npm test",
-    "npm run build"
-  ]
+  commands: commands
 )
-
-if ENV["BUILDKITE_TAG"]
-  puts "Triggered by tag: #{ENV["BUILDKITE_TAG"].sub("v", "")}"
-end
 
 puts pipeline.to_json
