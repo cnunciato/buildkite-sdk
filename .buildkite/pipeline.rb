0a0b81eac47a0c50d87a82126906611f97c7646b
Christian Nunciato <chris@nunciato.org>
2025-01-07 10:57:26 -0800

    Test steps in parallel

--- .buildkite/pipeline.rb
+++ .buildkite/pipeline.rb
@@ -1,23 +1,23 @@
-require_relative("../sdk/ruby/lib/buildkite.rb")
-require_relative("../sdk/ruby/lib/environment.rb")
+require_relative("../sdk/ruby/lib/buildkite")
+require_relative("../sdk/ruby/lib/environment")
 
 pipeline = Buildkite::Pipeline.new
-tag = ENV[Environment::BUILDKITE_TAG]
 
-commands = [
-  "npm test",
-  "npm run build",
-  "npm run docs",
-  "npm run apps",
-]
-
-if not tag.nil? and tag.start_with?("v")
-  commands.push("npm run publish")
+%w[test build apps].each do |task|
+  pipeline.add_step(
+    id: task,
+    label: ":hammer_and_wrench: #{task}",
+    command: "npm run #{task}"
+  )
 end
 
-pipeline.add_command_step(
-  label: ":hammer_and_wrench: Install, test, build, publish",
-  commands: commands,
-)
+tag = ENV[Environment::BUILDKITE_TAG]
+if !tag.nil? and tag.start_with?("v")
+  pipeline.add_step(
+    label: ":rocket: Publish",
+    command: "npm run publish",
+    depends_on: "build"
+  )
+end
 
 puts pipeline.to_json
